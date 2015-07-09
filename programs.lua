4963469030e7118c34f4a3d63d5c1ac9c7305438
payonel <payonel@hotmail.com>
2015-07-09 07:13:31 -0700

    prepare initial description of psh and payo-bash projects

---
+++ programs.lua
@@ -0,0 +1,36 @@
+﻿{
+    ["psh"] =
+		{
+        
+				["files"] =
+				{
+        },
+        
+	 			["dependencies"] =
+				{          
+        },
+
+				["repo"] = "tree/master/psh",
+        ["name"] = "Payo Remote Shell",
+        ["description"] = "openos remote shell and remote copy utilities",
+        ["note"] = "Adds with psh, pcp, and pshd to your PATH. psh/pcp/pshd -h for more options",
+        ["authors"] = "payonel"
+    },
+
+    ["payo-bash"] =
+		{
+        ["files"] =
+				{
+        },
+
+        ["dependencies"] =
+				{            
+        },
+
+        ["repo"] = "tree/master/payo-bash",
+        ["name"] = "Payonel gnu-inspired utilties",
+        ["description"] = "A collection of gnu softoware inspired utilities for openos shell to provide a more bash-like experience",
+        ["note"] = "You must either add payo-bash to your /etc/rc.cfg, or run load-payo-bash on each fresh boot",
+        ["authors"] = "payonel"
+    },
+}
