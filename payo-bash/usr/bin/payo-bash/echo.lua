c211401fffac1a29b4b74b7d5ae2da465725ae9b
payonel <payonel@hotmail.com>
2015-07-12 12:54:47 -0700

    add error print for echo parse failure

--- payo-bash/usr/bin/payo-bash/echo.lua
+++ payo-bash/usr/bin/payo-bash/echo.lua
@@ -2,6 +2,11 @@ local util = require("payo-lib/argutil")
 
 local args, options, reason = util.parse(table.pack(...), {{'n'},{}});
 
+if (not args or not options) then
+  io.stderr:write(tostring(reason) .. '\n')
+  os.exit()
+end
+
 for i = 1, #args do
   if i > 1 then
     io.write(" ")
