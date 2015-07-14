97fe5369662393183fff6132d545da054cd09a62
payonel <payonel@hotmail.com>
2015-07-14 08:17:36 -0700

    add all tests to all-tests.lua

--- payo-tests/var/payo-tests/all-tests.lua
+++ payo-tests/var/payo-tests/all-tests.lua
@@ -1,15 +1,16 @@
 local tests =
 {
-  "argutil-test.lua", 
-  "argutil-test.lua", 
-  "argutil-test.lua", 
+  "tableutil-test.lua", 
+  "stringutil-test.lua", 
+  "config-test.lua", 
   "argutil-test.lua", 
 };
 
 local stringutil = require("payo-lib/stringutil")
 
-local pwd = stringutil.getParentDirectory(os.get("_"))
+local pwd = stringutil.getParentDirectory(os.getenv("_"))
 
 for _,test in ipairs(tests) do
+  print("Running test: " .. test)
   dofile(pwd .. test)
 end
