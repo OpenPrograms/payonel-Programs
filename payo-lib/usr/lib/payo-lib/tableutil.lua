9e888da9625441326f128998b3844c7cc2c992c0
payonel <payonel@hotmail.com>
2015-07-13 21:33:03 -0700

    add table utils and tests

---
+++ payo-lib/usr/lib/payo-lib/tableutil.lua
@@ -0,0 +1,54 @@
+local util = {};
+
+function util.equal(t1, t2)
+  if (not t1 ~= not t2) then
+    return false, "not equally nil"
+  end
+
+  if (t1 == nil and t2 == nil) then
+    return true;
+  end
+
+  local t1_keys = {}
+  local t2_keys = {}
+  local key_diff = 0;
+
+  for k,_ in pairs(t1) do
+    t1_keys[k] = true;
+    key_diff = key_diff + 1;
+  end
+
+  for k,_ in pairs(t2) do
+    t2_keys[k] = true
+    key_diff = key_diff - 1;
+  end
+
+  if (key_diff ~= 0) then
+    return false, "different number of keys"
+  end
+
+  for k,_ in pairs(t1_keys) do
+    t2_keys[k] = nil;
+  end
+
+  if (next(t2_keys)) then
+    return false, "different keys"
+  end
+
+  for k,v in pairs(t1) do
+    if (type(t1[k]) ~= type(t2[k])) then
+      return false, string.format("in key [\"%s\"]: values not same type: %s ~= %s", tostring(k), type(t1[k]), type(t2[k]))
+    elseif (type(t1[k]) == type({})) then
+      local ok, reason = util.equal(t1[k], t2[k])
+      if (not ok) then
+        return false, string.format("in key [\"%s\"]: ", tostring(k), tostring(reason))
+      end
+    elseif (t1[k] ~= t2[k]) then
+      return false, string.format("in key [\"%s\"]: values not equal: %s ~= %s", tostring(k), tostring(t1[k]), tostring(t2[k]))
+    end
+  end
+
+  return true
+end
+
+return util;
---
+++ payo-tests/var/payo-tests/tableutil-test.lua
@@ -0,0 +1,33 @@
+local ser = require("serialization").serialize
+
+local lib = "payo-lib/tableutil"
+package.loaded[lib] = nil
+local util = require(lib)
+
+if (not util) then
+  error("failed to load " .. lib)
+end
+
+local function test(a, b, pass)
+  local ok, reason = util.equal(a, b) and util.equal(b, a);
+  if (ok and not pass or not ok and pass) then
+    io.stderr:write(string.format("%s ~= %s: %s", ser(a), ser(b), tostring(reason)))
+  end
+end
+
+test({}, {}, true)
+test(nil, {}, false)
+test({1}, {1}, true)
+test({1,2}, {1,2}, true)
+test({1,2}, {2,1}, false)
+test({a=1,b=2}, {b=2,a=1}, true)
+test({'a','b',key=10},{key=10,'a','b'}, true)
+test({'a','b',key=10},{key=11,'a','b'}, false)
+test({'a','b',key=10},{keys=10,'a','b'}, false)
+test({'a','b',key=10},{keys=10,'b','a'}, false)
+test({'a',{'b'}},{'a',{'b'}}, true)
+test({'a',{'b'}},{{'b'},'a'}, false)
+
+test(0, {}, false)
+test("", {}, false)
+test(function()end, {}, false)
