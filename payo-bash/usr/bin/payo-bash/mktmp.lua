04d3d38f2d20476bfd8c65c370610cdd021882de
payonel <payonel@hotmail.com>
2015-07-30 14:18:02 -0700

    give verbose option to mktmp for testing and possibly command line consumption

--- payo-bash/usr/bin/payo-bash/mktmp.lua
+++ payo-bash/usr/bin/payo-bash/mktmp.lua
@@ -1,6 +1,9 @@
 local fs = require("filesystem");
 local guid = require("payo-lib/guid");
 local touch = loadfile("/usr/bin/touch.lua");
+local argutil = require("payo-lib/argutil");
+
+local args, ops = argutil.parse(table.pack(...))
 
 if (not fs or not guid or not touch) then
   return nil, "missing tools for mktmp";
@@ -10,6 +13,11 @@ while (true) do
   local tmp = "/tmp/" .. guid.next();
   if (not fs.exists(tmp)) then
     touch(tmp);
+
+    if (ops.v or ops.verbose) then
+      io.write(tmp);
+    end
+
     return tmp;
   end
 end
