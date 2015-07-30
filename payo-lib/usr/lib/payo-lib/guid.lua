9450cbef726a2e0f7fbd7c32ae337f9a8f6c5a40
payonel <payonel@hotmail.com>
2015-07-30 14:06:15 -0700

    add various hex tests and give hex convert a first pass attempt

--- payo-lib/usr/lib/payo-lib/guid.lua
+++ payo-lib/usr/lib/payo-lib/guid.lua
@@ -2,11 +2,44 @@
 
 local util = {};
 
-function util.toHex(c)
-  return c;
+function util.toHex(n)
+  if (type(n) ~= type(0)) then
+    return nil, "toHex only converts numbers to strings"
+  end
+
+  local hexchars = "0123456789abcdef";
+  local result = "";
+  if (n < 0) then
+    result = "-";
+    n = -n;
+  end
+
+  while (n > 0) do
+    local next = math.floor(n % 16);
+    n = math.floor(n / 16);
+    result = result .. hexchars:sub(next, next);
+  end
+
+  return result;
 end
 
 function util.next()
+  -- e.g. 3c44c8a9-0613-46a2-ad33-97b6ba2e9d9a
+  -- 8-4-4-4-12
+  local sets = {8, 4, 4, 12};
+  local result = "";
+
+  local i;
+  for _,set in ipairs(sets) do
+    if (result:len() > 0) then
+      result = result .. "-";
+    end
+    for i = 1,set do
+      result = result .. util.toHex(math.random(0, 15));
+    end
+  end
+
+  return result;
 end
 
 return util;
--- payo-tests/var/payo-tests/guid-test.lua
+++ payo-tests/var/payo-tests/guid-test.lua
@@ -6,3 +6,33 @@ if (not util) then
   error("failed to load " .. lib)
 end
 
+local function are_equal(a, b, msg)
+  if (a ~= b) then
+    io.stderr:write(string.format("%s~=%s:%s\n", tostring(a), tostring(b), msg));
+  end
+end
+
+are_equal(util.toHex(0), '0', '0 hex');
+are_equal(util.toHex(1), '1', '1 hex');
+are_equal(util.toHex(2), '2', '2 hex');
+are_equal(util.toHex(3), '3', '3 hex');
+are_equal(util.toHex(4), '4', '4 hex');
+are_equal(util.toHex(5), '5', '5 hex');
+are_equal(util.toHex(6), '6', '6 hex');
+are_equal(util.toHex(7), '7', '7 hex');
+are_equal(util.toHex(8), '8', '8 hex');
+are_equal(util.toHex(9), '9', '9 hex');
+are_equal(util.toHex(10), 'a', '10 hex');
+are_equal(util.toHex(11), 'b', '11 hex');
+are_equal(util.toHex(12), 'c', '12 hex');
+are_equal(util.toHex(13), 'd', '13 hex');
+are_equal(util.toHex(14), 'e', '14 hex');
+are_equal(util.toHex(15), 'f', '15 hex');
+
+are_equal(util.toHex(-1), nil, '-1 hex');
+are_equal(util.toHex(''), nil, 'string hex');
+are_equal(util.toHex(nil), nil, 'nil hex');
+
+-- testing large values
+are_equal(util.toHex(4294907295), 'FFFF159F', '4294907295 hex');
+
