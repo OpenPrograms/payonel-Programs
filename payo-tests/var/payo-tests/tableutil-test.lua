f1cf6e6e6ad4b5f699d502e08e56851167be8fc0
payonel <payonel@hotmail.com>
2015-07-30 18:08:10 -0700

    dofile, not doile in tableutil test

--- payo-tests/var/payo-tests/tableutil-test.lua
+++ payo-tests/var/payo-tests/tableutil-test.lua
@@ -1,4 +1,4 @@
-local testutil = doile("/var/payo-tests/testutil.lua");
+local testutil = dofile("/var/payo-tests/testutil.lua");
 local util = testutil.load("payo-lib/tableutil");
 local ser = require("serialization").serialize
 
