9718ff726b81a97ea9b81031f183550cb248503f
payonel <payonel@hotmail.com>
2015-09-13 22:45:54 -0700

    add color coloring for status and move daemon code to lib. add start stop methods

--- psh/usr/bin/pshd-passive.lua
+++ psh/usr/bin/pshd-passive.lua
@@ -26,8 +26,68 @@ end
 
 local screenWidth = gpu and gpu.getResolution() or 0
 
+local function serviceStatusPrint(starColor, msg, callback, statusMsgOk, statusMsgFail)
+  local spacem = '  '
+
+  io.write(spacem)
+  setColor(starColor)
+  io.write('*')
+  revertColor()
+  io.write(spacem)
+
+  io.write(msg)
+
+  local bCallbackResult = true
+  local additionalMessages = {}
+  if callback ~= nil then
+    if type(callback) == "boolean" then
+      bCallbackResult = callback
+    else
+      local result = table.pack(callback())
+      bCallbackResult = table.remove(result, 1)
+      additionalMessages = result
+    end
+  end
+
+  if bCallbackResult and not statusMsgOk then
+    return
+  end
+
+  if not bCallbackResult and not statusMsgFail then
+    return
+  end
+
+  local statusColor = bCallbackResult and 0x00FF00 or 0xFF0000
+  local statusMsg   = bCallbackResult and statusMsgOk or statusMsgFail
+
+  local startMsgLen = spacem:len() * 2 + 1 + msg:len()
+
+  local openm = '[' .. spacem
+  local closem = spacem .. '] '
+  local numSpaces = 1
+  local slen = openm:len() + statusMsg:len() + closem:len()
+
+  numSpaces = math.max(1, screenWidth - startMsgLen - slen)
+  io.write(string.rep(' ', numSpaces))
+
+  setColor(0x0000FF)
+  io.write(openm)
+  setColor(statusColor)
+  io.write(statusMsg)
+  setColor(0x0000FF)
+  io.write(closem)
+  revertColor()
+
+  -- if additional messages were returned by the callback
+  for i,m in ipairs(additionalMessages) do
+    serviceStatusPrint(0xFF0000, m)
+  end
+end
+
 local function usage(msg)
-  io.stderr:write(msg .. "\nUsage: pshd [start|stop|status]\n")
+  serviceStatusPrint(0xFF0000, msg)
+  io.write('\n')
+  serviceStatusPrint(0xFFFF00, "Usage: pshd [start|stop|status]")
   os.exit()
 end
 
@@ -39,25 +99,23 @@ if not command then -- run in interactive mode
 elseif #args > 0 then
   usage("too many arguments")
 elseif command == "status" then
-  local status = psh_lib.pshd_running and "started" or "stopped"
-  local scolor = psh_lib.pshd_running and 0x00FF00 or 0xFF0000
-  local spacem = '  '
-  local openm = '[' .. spacem
-  local closem = spacem .. '] '
-  io.write(spacem .. 'pshd')
-  local numSpaces = 1
-  local slen = openm:len() + closem:len() + status:len()
-  numSpaces = math.max(1, screenWidth - spacem:len() - 4 - slen)
-  io.write(string.rep(' ', numSpaces))
-  setColor(0x0000FF)
-  io.write(openm)
-  setColor(scolor)
-  io.write(status)
-  setColor(0x0000FF)
-  io.write(closem)
-  revertColor()
+  serviceStatusPrint(0x00FF00, "pshd", psh_lib.pshd_running, "started", "stopped")
 elseif command == "start" then
+  if psh_lib.pshd_running then
+    serviceStatusPrint(0xFFFF00, "WARNING: pshd has already been started")
+  else
+    serviceStatusPrint(0x00FF00, "Starting pshd ...", psh_lib.start, "ok", "failed")
+  end
 elseif command == "stop" then
+  if not psh_lib.pshd_running then
+    serviceStatusPrint(0xFFFF00, "WARNING: pshd is already stopped")
+  else
+    serviceStatusPrint(0x00FF00, "Stopping pshd ...", psh_lib.stop, "ok", "failed")
+  end
 else
   usage("Unknown command parameter: " .. command)
+end
+
+if bInteractiveMode then
+  usage("Interactive mode is not yet implemented")
 end
--- psh/usr/lib/psh/init.lua
+++ psh/usr/lib/psh/init.lua
@@ -11,5 +11,41 @@ psh_cfg = psh_cfg or {}; -- simplify config checks later on
 
 local lib = {}
 lib.pshd_running = false
+lib.daemon = {}
+lib.daemon.handlers = {}
+
+function lib.start()
+  local l = event.listen
+
+  if lib.pshd_running then
+    return false, "pshd already running"
+  end
+
+  for token,handler in pairs(lib.daemon.handlers) do
+    if not l(token, handler) then
+      return false, "failed to register pshd daemon handler for " .. tostring(token)
+    end
+  end
+
+  lib.pshd_running = true
+  return m.open(1)
+end
+
+function lib.stop()
+  local ig = event.ignore
+
+  for token,handler in pairs(lib.daemon.handlers) do
+    if not ig(token, handler) then
+      return false, "failed to unregister pshd daemon handler for " .. tostring(token)
+    end
+  end
+
+  lib.pshd_running = false
+  return m.close(1)
+end
+
+lib.daemon.handlers.modem_message = function(...)
+  print("psh_lib.daemon.handlers.modem_message ", ...)
+end
 
 return lib
