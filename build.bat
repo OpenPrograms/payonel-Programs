9f4a531d9fb42b1e343e56fdf91aa6809bea5ed1
payonel <payonel@hotmail.com>
2015-10-03 00:09:07 -0700

    use colon and not equal sign for correct syntax for xcopy

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 SET target=D:\gitrepos\OCEmu\extras\cmd\a\e05fbfd8-f5ba-48d6-bf27-dd698803215d\
 SET CP=C:\Windows\System32\xcopy.exe
 
-%CP% /s /v /y /EXCLUDE=xcopy-excludes.txt OpenOS %target%
+%CP% /s /v /y /EXCLUDE:xcopy-excludes.txt OpenOS %target%
 %CP% /s /v /y payo-bash %target%
 %CP% /s /v /y payo-lib %target%
 REM %CP% /s /v /y payo-persistent-links %target%
