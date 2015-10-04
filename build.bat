4ee33040481f6c5c1ac93198db0cb2785621795f
payonel <payonel@hotmail.com>
2015-10-04 10:21:48 -0700

    provide instance paths by input file rather than build script

--- build.bat
+++ build.bat
@@ -1,11 +1,13 @@
-SET target=D:\gitrepos\OCEmu\extras\cmd\a\e05fbfd8-f5ba-48d6-bf27-dd698803215d\
+SET target_prefix=D:\gitrepos\OCEmu\extras\cmd
 SET CP=C:\Windows\System32\xcopy.exe
 
-%CP% /s /v /y /EXCLUDE:xcopy-excludes.txt OpenOS %target%
-%CP% /s /v /y payo-bash %target%
-%CP% /s /v /y payo-lib %target%
-REM %CP% /s /v /y payo-persistent-links %target%
-%CP% /s /v /y payo-tests %target%
-%CP% /s /v /y popm %target%
-%CP% /s /v /y psh %target%
-
+FOR /F "tokens=*" %%a in (%target_prefix%\instances.txt) do (
+  SET target=%target_prefix%\%%a
+  REM %CP% /s /v /y /EXCLUDE:xcopy-excludes.txt OpenOS %target%
+  REM %CP% /s /v /y payo-bash %target%
+  REM %CP% /s /v /y payo-lib %target%
+  REM %CP% /s /v /y payo-persistent-links %target%
+  REM %CP% /s /v /y payo-tests %target%
+  REM %CP% /s /v /y popm %target%
+  REM %CP% /s /v /y psh %target%
+)
