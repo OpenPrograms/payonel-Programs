87a6fcc667ab225b6a703bf1c197aa47c9f37497
payonel <payonel@hotmail.com>
2015-10-04 10:48:40 -0700

    fix build script to copy to correct instance paths - windows batch is rubbish

--- build.bat
+++ build.bat
@@ -2,12 +2,11 @@ SET target_prefix=D:\gitrepos\OCEmu\extras\cmd
 SET CP=C:\Windows\System32\xcopy.exe
 
 FOR /F "tokens=*" %%a in (%target_prefix%\instances.txt) do (
-  SET target=%target_prefix%\%%a
-  REM %CP% /s /v /y /EXCLUDE:xcopy-excludes.txt OpenOS %target%
-  REM %CP% /s /v /y payo-bash %target%
-  REM %CP% /s /v /y payo-lib %target%
-  REM %CP% /s /v /y payo-persistent-links %target%
-  REM %CP% /s /v /y payo-tests %target%
-  REM %CP% /s /v /y popm %target%
-  REM %CP% /s /v /y psh %target%
+  %CP% /s /v /y /EXCLUDE:xcopy-excludes.txt OpenOS %target_prefix%\%%a
+  %CP% /s /v /y payo-bash %target_prefix%\%%a
+  %CP% /s /v /y payo-lib %target_prefix%\%%a
+  %CP% /s /v /y payo-persistent-links %target_prefix%\%%a
+  %CP% /s /v /y payo-tests %target_prefix%\%%a
+  %CP% /s /v /y popm %target_prefix%\%%a
+  %CP% /s /v /y psh %target_prefix%\%%a
 )
