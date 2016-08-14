41dcad64a51016ce31bceec3250df09c22d59e92
payonel <payonel@hotmail.com>
2016-08-13 22:34:44 -0700

    returning to work on psh

--- build.bat
+++ build.bat
@@ -5,9 +5,9 @@ FOR /F "tokens=*" %%a in (%target_prefix%\instances.txt) do (
 REM  %CP% /s /v /y /EXCLUDE:xcopy-excludes.txt SecureOS %target_prefix%\%%a
   %CP% /s /v /y /EXCLUDE:xcopy-excludes.txt openos %target_prefix%\%%a
 REM  %CP% /s /v /y payo-bash %target_prefix%\%%a
-REM  %CP% /s /v /y payo-lib %target_prefix%\%%a
+  %CP% /s /v /y payo-lib %target_prefix%\%%a
 REM  %CP% /s /v /y payo-persistent-links %target_prefix%\%%a
   %CP% /s /v /y payo-tests %target_prefix%\%%a
 REM  %CP% /s /v /y popm %target_prefix%\%%a
-REM  %CP% /s /v /y psh %target_prefix%\%%a
+  %CP% /s /v /y psh %target_prefix%\%%a
 )
