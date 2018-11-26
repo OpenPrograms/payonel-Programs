898122f896e44074eb4e14bc9d87b22731f25fc1
payonel <payonel@hotmail.com>
2018-11-25 23:52:39 -0800

    more host code working, fake stream

--- psh/usr/lib/psh/host.lua
+++ psh/usr/lib/psh/host.lua
@@ -1,6 +1,8 @@
 local ser = require("serialization").serialize
 local dsr = require("serialization").unserialize
 local shell = require("shell")
+local buffer = require("buffer")
+local process = require("process")
 
 local H = {}
 
@@ -9,6 +11,7 @@ local _init_packet_timeout = 5
 local packets =
 {
   init = {label = "psh.init"},
+  output = {label = "psh.io.write"},
 }
 
 function packets.init.parse(packet_label, packet_body)
@@ -16,40 +19,60 @@ function packets.init.parse(packet_label, packet_body)
   return {
     command = packet_body[1] or "/bin/sh",
     timeout = packet_body[2] or _init_packet_timeout,
+    X = packet_body[3] or false
   }
 end
 
-local function next_packet(client, timeout)
-  local packet = table.pack(client:pull(timeout))
+local function next_packet(socket, timeout)
+  local packet = table.pack(socket:pull(timeout))
   assert(packet.n > 0)
   return packet[1], dsr(packet[2])
 end
 
-function H.run(client)
-  -- local o = io.output()
+local function new_stream(socket, forward_gpu)
+  local stream = {handle = socket}
 
-  local ok, why = pcall(function()
-    -- the client connection hasn't proven it is for psh
+  function stream:write(...)
+    local buf = table.concat({...})
+    return self.handle:push(packets.output.label, buf)
+  end
+
+  function stream:read(...)
+  end
+
+  function stream:close()
+    self.handle:close()
+  end
+
+  local bs = buffer.new("rw", stream)
+  bs.tty = true
+  bs:setvbuf("no")
+  process.closeOnExit(bs)
+
+  return bs
+end
+
+function H.run(socket)
+  pcall(function()
+    -- the socket connection hasn't proven it is for psh
     -- though it is using psh.sockets
     -- give it time to provide the init packet to establish a psh session
-    local init_packet = packets.init.parse(next_packet(client, _init_packet_timeout))
-    local context = {socket = client}
+    local init_packet = packets.init.parse(next_packet(socket, _init_packet_timeout))
+    local context = {socket = socket}
     context.command = init_packet.command
     context.timeout = init_packet.timeout
 
-    local host_stream = setmetatable({}, {__metatable = "file"})
+    local host_stream = new_stream(socket, context.X)
     io.input(host_stream)
     io.output(host_stream)
     io.error(host_stream)
 
     shell.getShell()(nil, context.command)
-  end)
-  client:close()
 
-  -- o = io.dup(o)
-  -- getmetatable(o).__metatable = "file"
-  -- io.output(o)
-  -- print(ok, why)
+    host_stream:flush()
+
+  end)
+  socket:close()
 end
 
 return H
